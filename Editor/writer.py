6c536ac13c900a6efe138e489c64a9cbd10ae9a6
ngawangtrinley <ngawangtrinley@gmail.com>
2017-01-02 10:43:35 +0800

    cleaned up files

--- Editor/writer.py
+++ Editor/writer.py
@@ -7,8 +7,7 @@ from PyQt5.QtGui import (QFont, QIcon, QKeySequence, QSyntaxHighlighter, QPixmap
                          QTextCharFormat, QTextCursor, QTextTableFormat)
 from PyQt5.QtWidgets import (QStyleFactory, QAction, QApplication, QFileDialog, QMainWindow, QMenu, QLabel, QLineEdit, QGridLayout, QMenuBar,
                              QCheckBox, QTableView, QRadioButton, QListWidgetItem, QListView, QWidget, QPushButton, QDockWidget, QDialog, QListWidget, QMessageBox, QTextEdit)
-from ext import *
-
+import highlighter
 
 class MainWindow(QMainWindow):
 
@@ -19,7 +18,7 @@ class MainWindow(QMainWindow):
 # Editor
     def setupEditor(self):
         font = QFont()
-        # font.setFamily('Noto Sans Tibetan')
+        font.setFamily('Noto Sans Tibetan')
         font.setFixedPitch(True)
         font.setPointSize(10)
 
@@ -67,90 +66,18 @@ class MainWindow(QMainWindow):
 
         self.menuBarRight = QMenuBar(self.menu)
         self.menu.setCornerWidget(self.menuBarRight, Qt.TopRightCorner)
-        # Workspace
-        self.workspaceMenu = self.menuBarRight.addMenu("Workspace")
-        # self.workspaceMenu.setLayoutDirection(Qt.RightToLeft)
-        # self.viewMenu.clear()
 
     def initUI(self):
         self.createActions()
         self.createMenus()
         self.createToolbar()
-        self.createDockWidgets()
         self.setupEditor()
         self.createStatusBar()
         self.setCentralWidget(self.editor)
         self.setWindowTitle("BDRC Editor")
-        self.setWindowIcon(QIcon("images/tab1.png"))
+        self.setWindowIcon(QIcon("tab1.png"))
         self.setWindowState(Qt.WindowMaximized)
         self.resize(1200, 480)
-
-# DockWidgets
-    def createSearchWidget(self):
-
-        self.dockSearch = QDockWidget("  Search", self)
-        self.dockWidgetContents = QWidget()
-
-        self.searchGrid = QGridLayout(self.dockWidgetContents)
-        self.searchGrid.setContentsMargins(10, 5, 10, 10)
-
-        self.lineEdit = QLineEdit(self.dockWidgetContents)
-        self.searchGrid.addWidget(self.lineEdit, 0, 0, 1, 1)
-
-        self.searchButton = QPushButton(self.dockWidgetContents)
-        self.searchGrid.addWidget(self.searchButton, 0, 1, 1, 1)
-        self.searchButton.setText('Search')
-
-        self.fileRadio = QRadioButton("Document")
-        self.searchGrid.addWidget(self.fileRadio, 1, 1, 1, 1)
-        self.fileRadio.setChecked(True)
-
-        self.folderRadio = QRadioButton("Folder (all UTF-8 files)")
-        self.searchGrid.addWidget(self.folderRadio, 1, 0, 1, 1)
-
-        self.searchTableView = QTableView(self.dockWidgetContents)
-        self.searchGrid.addWidget(self.searchTableView, 2, 0, 1, 2)
-
-        self.dockSearch.setWidget(self.dockWidgetContents)
-        self.addDockWidget(Qt.DockWidgetArea(1), self.dockSearch)
-
-        self.viewMenu.addAction(self.dockSearch.toggleViewAction())
-
-    def createListLoaderWidget(self):
-        self.listLoaderDockWidget = QDockWidget("Word Lists", self)
-        self.listLoaderDockWidget.setAllowedAreas(
-            Qt.LeftDockWidgetArea | Qt.RightDockWidgetArea)
-
-        self.listLoaderWidget = QWidget()
-
-        self.listLoaderGrid = QGridLayout(self.listLoaderWidget)
-        self.listLoaderGrid.setContentsMargins(10, 5, 10, 10)
-
-        self.listButton = QPushButton('Add List')
-        self.listLoaderGrid.addWidget(self.listButton, 0, 0, 1, 3)
-
-        self.level1ListRadio = QCheckBox()
-        self.listLoaderGrid.addWidget(self.level1ListRadio, 1, 0, 1, 1)
-
-        self.level1ListPath = QLabel("Level 1 word list")
-        self.listLoaderGrid.addWidget(self.level1ListPath, 1, 1, 1, 1)
-
-        color = QPixmap(16, 16)
-        color.fill(Qt.blue)
-        self.colorLabel = QLabel()
-        self.colorLabel.setPixmap(color)
-        self.listLoaderGrid.addWidget(self.colorLabel, 1, 2, 1, 1)
-
-        self.widget = QWidget()
-        self.listLoaderGrid.addWidget(self.widget, 2, 0, 1, 1)
-
-        self.listLoaderDockWidget.setWidget(self.listLoaderWidget)
-        self.addDockWidget(Qt.RightDockWidgetArea, self.listLoaderDockWidget)
-        self.viewMenu.addAction(self.listLoaderDockWidget.toggleViewAction())
-
-    def createDockWidgets(self):
-        self.createSearchWidget()
-        self.createListLoaderWidget()
 
 # Actions
     def about(self):
@@ -211,23 +138,23 @@ class MainWindow(QMainWindow):
         # self.workspaceMenu.setText("h&a")
 
     def createActions(self):
-        self.newFileAction = QAction(QIcon('images/filenew.png'), "&New...",
+        self.newFileAction = QAction(QIcon('filenew.png'), "&New...",
                                   self, shortcut=QKeySequence.New,
                                   statusTip="Create a new file", triggered=self.newFile)
-        self.openFileAction = QAction(QIcon('images/fileopen.png'), "&Open...",
+        self.openFileAction = QAction(QIcon('fileopen.png'), "&Open...",
                                    self, shortcut=QKeySequence.Open,
                                    statusTip="Open a text file", triggered=self.openFile)
-        self.saveFileAction = QAction(QIcon('images/filesave.png'), "&Save...", self,
+        self.saveFileAction = QAction(QIcon('filesave.png'), "&Save...", self,
                                    shortcut=QKeySequence.Save,
                                    statusTip="Save the current document", triggered=self.saveFile)
-        self.undoAction = QAction(QIcon('images/editundo.png'), "&Undo", self,
+        self.undoAction = QAction(QIcon('editundo.png'), "&Undo", self,
                                shortcut=QKeySequence.Undo,
                                statusTip="Undo the last editing action", triggered=self.undo)
-        self.redoAction = QAction(QIcon('images/editredo.png'), "&Redo", self,
+        self.redoAction = QAction(QIcon('editredo.png'), "&Redo", self,
                                shortcut=QKeySequence.Redo,
                                statusTip="Redo the last editing action", triggered=self.redo)
-        self.segmentAction = QAction(QIcon('images/segment.png'), "&Segmenter", self,
-                                  shortcut="Ctrl+G",
+        self.segmentAction = QAction(QIcon('segment.png'), "&Segment", self,
+                                  shortcut="Ctrl+Shift+C",
                                   statusTip="Segment the current document", triggered=self.segment)
         self.actionQuit = QAction("&Quit", self, shortcut="Ctrl+Q",
                 triggered=self.close)
