7451f86f504c7ca068b6d06498c84ab5c075936f
Kevin <kevin.huang@nuwainfo.com>
2018-05-01 12:36:10 +0800

    add delete buttons

--- widgets/FindWidget.py
+++ widgets/FindWidget.py
@@ -1,7 +1,7 @@
-from collections import OrderedDict
+from functools import partial
 
-from PyQt5.QtCore import Qt, QStringListModel
-from PyQt5.QtGui import QTextCursor, QPalette
+from PyQt5.QtCore import Qt, QStringListModel, QSize
+from PyQt5.QtGui import QTextCursor, QPalette, QIcon
 from PyQt5.QtWidgets import (
     QWidget, QTextEdit, QCompleter, QComboBox, QPushButton,
     QLabel, QLineEdit, QFormLayout, QHBoxLayout, QVBoxLayout
@@ -12,37 +12,73 @@ class TokenWidget(QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.attrs = []
         self.initUI()
+
+    def initUI(self):
+         # Color
         pal = QPalette()
         pal.setColor(QPalette.Background, Qt.white)
         self.setAutoFillBackground(True)
         self.setPalette(pal)
 
-    def initUI(self):
-        hbox = QHBoxLayout()
-
-        comboBox = QComboBox()
-        comboBox.addItems(TokenWidget.ATTR_CHOICES)
-        comboBox.setStyleSheet("background-color: white;")
-        hbox.addWidget(comboBox)
-        hbox.addWidget(QLineEdit())
-
+        # Buttons
         self.addAttrButton = QPushButton('add attribute')
+
+        self.removeTokenButton = QPushButton()
+        self.removeTokenButton.setFlat(True)
+        self.removeTokenButton.setIconSize(QSize(25, 25))
+        self.removeTokenButton.setIcon(QIcon('files\\delete.png'))
+        self.removeTokenButton.clicked.connect(partial(self.removeToken, self))
+
         self.addAttrButton.clicked.connect(self.addAttr)
 
         self.fbox = QFormLayout()
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.addAttrButton, 3)
+        hbox.addWidget(self.removeTokenButton, 1)
         self.fbox.addRow(hbox)
-        self.fbox.addRow(self.addAttrButton)
 
+        self.addAttr()
         self.setLayout(self.fbox)
+
+    def removeToken(self, token):
+        self.addAttrButton.setParent(None)
+        self.removeTokenButton.setParent(None)
+
+        for attr in self.attrs:
+            attr.itemAt(2).widget().setParent(None)
+            attr.itemAt(1).widget().setParent(None)
+            attr.itemAt(0).widget().setParent(None)
+
+        self.setParent(None)
+
+    def removeAttr(self, attr):
+        attr.itemAt(2).widget().setParent(None)
+        attr.itemAt(1).widget().setParent(None)
+        attr.itemAt(0).widget().setParent(None)
+        attr.setParent(None)
+        self.attrs.remove(attr)
 
     def addAttr(self):
         hbox = QHBoxLayout()
+
         comboBox = QComboBox()
         comboBox.addItems(TokenWidget.ATTR_CHOICES)
+        
+        removeTokenButton = QPushButton()
+        removeTokenButton.setFlat(True)
+        removeTokenButton.setIconSize(QSize(25, 25))
+        removeTokenButton.setIcon(QIcon('files\\delete.png'))
+        removeTokenButton.clicked.connect(partial(self.removeAttr, hbox))
+
         hbox.addWidget(comboBox)
         hbox.addWidget(QLineEdit())
+        hbox.addWidget(removeTokenButton)
+
         self.fbox.insertRow(self.fbox.rowCount() - 1, hbox)
+        self.attrs.append(hbox)
+
 
 class FindWidget(QWidget):
     def __init__(self, parent):
