9284511adc941ca1076ed70371dc85f9028c72ca
Kevin <kevin.huang@nuwainfo.com>
2018-09-10 15:14:10 +0800

    clean new branches

--- widgets/__init__.py
+++
@@ -1,7 +0,0 @@
-from .CounterWidget import CounterWidget
-from .FindWidget import FindWidget
-from .ProfileWidget import ProfileWidget
-from .DictionaryWidget import DictionaryEditorWidget
-
-__all__ = ["CounterWidget", "FindWidget",
-           "ProfileWidget", "DictionaryEditorWidget"]
