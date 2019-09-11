0036cfbef6b6f36ed79810cc1b102913131355e0
ngawangtrinley <ngawangtrinley@gmail.com>
2019-09-11 23:31:04 +0800

    simple install

--- install.py
+++ install.py
@@ -1,2 +1,12 @@
-import pkg_resources
-pkg_resources.get_distribution("pandas ").version
+# coding:utf8
+import sys
+import subprocess
+
+
+if __name__ == "__main__":
+    print('Preparing Dakje ...')
+    subprocess.call(
+        [sys.executable, "-m", "pip", "install", "-r", "requirements.txt", "--user", "--upgrade"]
+    )
+    print('Done!')
+    print()
