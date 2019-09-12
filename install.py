f53271ef2eea32e79f1d2d7ccaca48a2cd1265e0
ngawangtrinley <ngawangtrinley@gmail.com>
2019-09-12 12:38:12 +0800

    add shortcut

--- install.py
+++ install.py
@@ -4,9 +4,36 @@ import subprocess
 
 
 if __name__ == "__main__":
-    print('Preparing Dakje ...')
+
+    # Check Python version
+    print('Checking your system ...')
+    print("Python Version: {0:d}.{1:d}.{2:d}".format(*sys.version_info[:3]))
+    if sys.version_info < (3, 5, 0) and sys.version_info[0] == 3:
+        print("Sorry, you must have Python 3.5.0 or higher.")
+        exit(5)
+
+    # upgrade pip
     subprocess.call(
-        [sys.executable, "-m", "pip", "install", "-r", "requirements.txt", "--user", "--upgrade"]
+        [sys.executable, "-m", "pip3", "install", "pip", "--user", "--upgrade"]
     )
+
+    # upgrade setuptools
+    subprocess.call(
+        [sys.executable, "-m", "pip3", "install", "setuptools", "--user", "--upgrade"]
+    )
+    print()
+
+    # get dependencies
+    print('Installing Dakje ...')
+    subprocess.call(
+        [sys.executable, "-m", "pip3", "install", "-r", "requirements.txt", "--user", "--upgrade"]
+    )
+
+    from shortcut import ShortCutter
+    s = ShortCutter()
+    s.create_desktop_shortcut("Editor.pyw")
+    s.create_menu_shortcut("Editor.pyw")
+
+    print()
     print('Done!')
     print()
