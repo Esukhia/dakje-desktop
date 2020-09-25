b01571672559cd6eb422ba471fe252902d6d237d
Casey <kehsin1214@gmail.com>
2020-09-25 19:55:02 +0800

    diff in tokenmanager

--- managers/TokenManager.py
+++ managers/TokenManager.py
@@ -3,17 +3,88 @@
 '''
 
 import os
+import time
 
 from pathlib import Path
+# from PyQt5.QtCore import QThread, pyqtSignal
 
-from widgets import Matchers
 from storage.models import Rule
 from storage.models import Token as TokenModel
 from .ViewManager import ViewManager
+from . import Matchers
 from web.settings import BASE_DIR
 from web.settings import FILES_DIR
 
 from horology import timed
+from diff_match_patch import diff
+
+# class TokenList(list):
+#     def __init__(self, data=None):
+#         self.tokens = list(data) if data else []
+
+#     def __repr__(self):
+#         return repr(self.tokens)
+#
+#     def __str__(self):
+#         return str(self.tokens)
+#
+#     def __getitem__(self, key):
+#         print('__getitem__')
+#         return super().__getitem__(key)
+# #         return self.tokens[i]
+#         if isinstance(key, slice):
+#             start, stop, step = key.indices(len(self))
+#             return TokenList([self[i] for i in range(start, stop, step)])
+#         else:
+#             return self.tokens[key]
+#
+#     def __len__(self):
+#         return len(self.tokens)
+#
+# #     def __delitem__(self, i):
+# #         print('__delitem__')
+# #         del self.tokens[i]
+#
+#     def __setitem__(self, key, val):
+#         # optional: self._acl_check(val)
+#         # abc
+#         # 012
+#         # a12345bc
+#         # 01  4 67
+#
+#         print('__setitem__')
+#         if isinstance(key, slice):
+#             start, stop, step = key.indices(len(self))
+#             self.tokens[start:stop:step] = val
+#             tokensStart = start
+#         else:
+#             self.tokens[key] = val
+#             tokensStart = key
+#
+#         lenOfTokens = (self.tokens[tokensStart:])
+#         for i, token in enumerate(self.tokens[tokensStart:]):
+#             lenOfEachTokenText = len(token.text)
+#             if i == 0: # 更改處的開始
+#                 # 上一個 end
+#                 token.start = self.tokens[tokensStart-1].end + 1
+#                 token.end = token.start + lenOfEachTokenText + ' '
+#             else:
+#                 token.start = token.end
+#                 token.end = (token.start + lenOfEachTokenText + ' '
+#                              if i != lenOfTokens - 1
+#                              else token.start + lenOfEachTokenText)
+
+
+
+#     def insert(self, key, val):
+#         # optional: self._acl_check(val)
+#         print('insert')
+#         self.tokens.insert(key, val)
+
+
+#     def extend(self, list):
+#         self.tokens.extend(list)
+
 
 class Token:
     def __init__(self, token, id=None):
@@ -22,9 +93,9 @@ class Token:
         (pybo token attributes + custom attributes)
         IMPORTANT: .text_unaffixed should be used for lookups
         .text might include extra tseks
-            :param self: 
-            :param token: 
-            :param id=None: 
+            :param self:
+            :param token:
+            :param id=None:
         """
         # copy all token attributes from pybo tokens
         self.text = token.text
@@ -33,18 +104,19 @@ class Token:
         self.lemma = token.lemma
         self.pos = token.pos
         self.start = token.start        # index from the source string
-        self.type = token.chunk_type    
+        self.type = token.chunk_type
         # self.sense = token.sense      # no sense in pybo
         self.level = None
         self.sense = None
 
         self.id = id  # have no id before save to database
 
-        self.blockIndex = None
+#         self.blockIndex = None
         self.end = self.start + len(self.text)
         self.string = None
 
     # @timed(unit='ms')
+
     def applyTokenModel(self, tokenModel):
         # add attributes from the DB
         self.pos = tokenModel.pos if tokenModel.pos else self.pos
@@ -53,12 +125,15 @@ class Token:
         self.sense = tokenModel.sense if tokenModel.sense else self.sense
 
     @property
+
     def length(self):
         return self.end - self.start
 
     @property
+
     def textWithoutTsek(self):
-        return self.text_unaffixed[:-1] if self.text_unaffixed.endswith('་') else self.text_unaffixed
+        return (self.text_unaffixed[:-1] if self.text_unaffixed.endswith('་')
+                                         else self.text_unaffixed)
 
 class TokenManager:
     # This class runs botok, and gives him custom lists
@@ -84,67 +159,89 @@ class TokenManager:
         # self.mode = "default"
         # self.tagger = None
         self.matcher = Matchers.expertaRuleMatcher()
+#         from storage.models import Token as TokenModel
+#         with open(self.TRIE_ADD_TEMP_FILE, 'w', encoding="utf-8") as f:
+#             f.write('\n'.join([
+#                 '{} {}'.format(d.text, d.pos)
+#                 for d in TokenModel.objects.filter(
+#                     type=TokenModel.TYPE_UPDATE) if d.pos is not None
+#             ]))
+#
+#         with open(self.TRIE_DEL_TEMP_FILE, 'w', encoding="utf-8") as f:
+#             f.write('\n'.join([
+#                 '{} {}'.format(d.text, d.pos)
+#                 for d in TokenModel.objects.filter(
+#                     type=TokenModel.TYPE_REMOVE) if d.pos is not None
+#             ]))
 
-        with open(self.TRIE_ADD_TEMP_FILE, 'w', encoding="utf-8") as f:
-            f.write('\n'.join([
-                '{} {}'.format(d.text, d.pos)
-                for d in TokenModel.objects.filter(
-                    type=TokenModel.TYPE_UPDATE) if d.pos is not None
-            ]))
-
-        with open(self.TRIE_DEL_TEMP_FILE, 'w', encoding="utf-8") as f:
-            f.write('\n'.join([
-                '{} {}'.format(d.text, d.pos)
-                for d in TokenModel.objects.filter(
-                    type=TokenModel.TYPE_REMOVE) if d.pos is not None
-            ]))
+        # filter tokens added to the db
+        tokenModels = TokenModel.objects.filter(
+            type=TokenModel.TYPE_UPDATE)
+        # import db tokens into tokenDict
+        self.tokenDict = {
+            tokenModel.text: tokenModel for tokenModel in tokenModels}
 
 
     @property
+
     def view(self):
         return self.editor.view
 
     @property
+
     def tokens(self):
         return self.editor.tokens
 
     @timed(unit='ms', name='TokenManager.segment: ')
+
     def segment(self, string):
 
         tokens = self.editor.tokenizer.tokenize(string, spaces_as_punct=True)
 
+#         return TokenList([Token(t) for t in tokens])
         return [Token(t) for t in tokens]
 
+    # create display string for textEdit
+    def _join(self, tokens, toStr, sep=''):
+#         blockIndex = 0
+        result = ''
+        for i, token in enumerate(tokens):
+#             token.blockIndex = blockIndex
+            token.start = len(result)
+            result += (toStr(token) + sep if i != len(tokens) - 1
+                       else toStr(token))
+            token.end = len(result)
+#             if token.text.endswith(('\n', ' ')):
+#                 blockIndex += 1
+#         print(f'_join: {result}, block: {blockIndex}')
+        return result
+
+#     def _join(self, tokens, toStr, sep):
+#         result = []
+#         lenOfTokens = len(tokens)
+#         for i, token in enumerate(tokens):
+#             result.append((toStr(token) + sep) if i != lenOfTokens - 1
+#                        else toStr(token))
+#         result = ''.join(result)
+#
+#         return result
+
+    @timed(unit='ms', name='getString: ')
     def getString(self):
-        # create display string for textEdit
-        def _join(tokens, toStr, sep):
-            blockIndex = 0
-            result = ''
-            for i, token in enumerate(tokens):
-                token.blockIndex = blockIndex
-                token.start = len(result)
-                result += (toStr(token) + sep if i != len(tokens) - 1
-                           else toStr(token))
-                token.end = len(result)
-
-                if token.text.endswith(('\n', ' ')):
-                    blockIndex += 1
-            print(f'_join: {result}, block: {blockIndex}')
-            return result
-
         if self.view == ViewManager.PLAIN_TEXT_VIEW:
-            return _join(self.tokens, lambda t: t.text, sep='')
+            return self._join(self.tokens, lambda t: t.text, sep='')
 
         elif self.view == ViewManager.SPACE_VIEW:
-            return _join(self.tokens, lambda t: t.text, sep=' ')
+            return self._join(self.tokens, lambda t: t.text, sep=' ')
 
         elif self.view == ViewManager.TAG_VIEW:
-        # virtual default tag+space, tag and space should be 
+        # virtual default tag+space, tag and space should be
         #     return _join(self.tokens, lambda t: t.text + '࿚' + t.pos, sep='')
-            return _join(self.tokens, lambda t: t.text + '࿚' + t.pos, sep='  ')
+            return self._join(self.tokens, lambda t: t.text + '࿚' + t.pos, sep='  ')
         else:
-            return _join(self.tokens, lambda t: t.text + '࿚' + t.pos, sep='  ')
+            return self._join(self.tokens, lambda t: t.text + '࿚' + t.pos, sep='  ')
 
+    @timed(unit='ms')
     def find(self, position):
         for i, token in enumerate(self.tokens):
             if position in range(token.start, token.end):
@@ -168,19 +265,89 @@ class TokenManager:
         pass
 
     @timed(unit='ms')
-    def applyDict(self):
-        # filter tokens added to the db 
-        tokenModels = TokenModel.objects.filter(
-            type=TokenModel.TYPE_UPDATE)
-        # import db tokens into tokenDict
-        tokenDict = {
-            tokenModel.text: tokenModel for tokenModel in tokenModels}
+    def applyDict(self, startToken=None, endToken=None):
+        if startToken and endToken:
+            tokens = self.tokens[startToken:endToken + 1]
+        else:
+            tokens = self.tokens
 
-        for token in self.tokens:
-            tokenModel = tokenDict.get(token.text_unaffixed)
+        for token in tokens:
+            tokenModel = self.tokenDict.get(token.text_unaffixed)
 
             if tokenModel is None:
-                tokenModel = tokenDict.get(token.textWithoutTsek)
+                tokenModel = self.tokenDict.get(token.textWithoutTsek)
 
             if tokenModel is not None:
                 token.applyTokenModel(tokenModel)
+
+    @timed(unit='ms', name='editor.diff: ')
+    def diff(self, tokens, oldText, newText):
+        #if (oldText and not tokens) or (not oldText and tokens):
+        #    raise RuntimeError("old text and tokens not consistent.")
+
+        changes = diff(oldText, newText, timelimit=0, checklines=False,
+                               counts_only=False)
+        if len(changes) == 1: #沒有改變
+            return 0, 0, ''
+        # changes = [('=', 'རྒྱ་ག'), ('-', 'ར'), ('=', '་སྐད་དུ། བོ་དྷི་ས་ཏྭ་ཙརྱ་ཨ་བ་ཏ་ར།')]
+        oldString = ''
+        newString = ''
+        for op, string in changes:
+            if op == "-" or op == "+":
+                if not sameStringLength:
+                    sameStringLength = 0
+                if op == "+":
+                    newString = newString + string
+                    lastWordIndex = (sameStringLength - 1)
+                    # 加在最後面時
+                    start = oldString.find('།', lastWordIndex)
+                    if start == -1: # 加在某處時
+                        newStringLength = len(newString)
+                        start = oldString.rfind('།', 0, newStringLength)
+                else:
+                    oldString = oldString + string
+                    newStringLength = len(newString)
+                    start = oldString.rfind('།', 0, newStringLength)
+
+                if start == -1: # 改的地方前面沒有'།'
+                    start = 0
+                changePos = oldString.find(string, sameStringLength - 1)
+            else:
+                sameStringLength = len(string)
+                oldString = oldString + string
+                newString = newString + string
+
+        # 往後 scan ，結束位置
+        endOld = oldString.find('།', changePos)
+        endNew = newString.find('།', changePos)
+
+        if tokens[-1].end == changePos: # 在文章最後面加字
+            tokenLength = len(tokens)
+            tokenStart, tokenEnd = tokenLength, tokenLength
+        else: # 修改處字串的開始到結束，找是 token 的哪裡開始到哪裡結束
+            i = 0
+            for e in tokens:
+                if e.start <= start and e.end >= start:
+                    if e.end == start:
+                        tokenStart = i + 1
+                        break
+                    else:
+                        tokenStart = i
+                        break
+                i += 1
+            i = 0
+            for e in tokens:
+                if e.start <= endOld and e.end >= endOld:
+                    lenOfToken = len(tokens)
+                    if i != (lenOfToken - 1):
+                        if tokens[i + 1].start == endOld:
+                            tokenEnd = i + 1
+                            break
+                        else:
+                            tokenEnd = i
+                            break
+                i += 1
+        # 前後 scan 得到的修改後字串
+        afterChangingString = newText[start: endNew + 1]
+
+        return tokenStart, tokenEnd, afterChangingString
